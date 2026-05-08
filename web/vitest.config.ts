7bfa330b9b708f8d85d43cd6bf13798e1e473ef8
Ivan <79221766+Erisuuuuu@users.noreply.github.com>
2026-05-08 15:46:02 +0300

    chore: exclude e2e from vitest

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     globals: true,
     setupFiles: ['./vitest.setup.ts'],
     include: ['__tests__/**/*.{test,spec}.{ts,tsx}'],
+    exclude: ['__tests__/e2e/**'],
   },
   resolve: {
     alias: {
